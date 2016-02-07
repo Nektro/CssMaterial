35bd81e7b3bd4e10c74c64a054ec91d7196bf70c
Nektro <nektromc@gmail.com>
2016-02-06 22:55:35 -0500

    pull-out navbar error fix and speed up

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 })();
 (function() {
   // slide out navbar
-  var a = document.getElementById("side-nav");
+  var a = document.getElementById("side-nav") | document.createElement('div');
   if (a.className === "pull-out") {
     document.getElementsByClassName("sidenavcontrol")[0].addEventListener('click', function(e) {
       var f = function() {
         var b = window.getComputedStyle(a, null).getPropertyValue("left");
         var l = parseInt(b.substring(0,b.length-2));
-        a.style.left = (l+20) + "px";
+        a.style.left = (l+30) + "px";
         if (l < 240) {
           requestAnimationFrame(f);
         }
@@ -26,7 +26,7 @@
           var f = function() {
             var b = window.getComputedStyle(a, null).getPropertyValue("left");
             var l = parseInt(b.substring(0,b.length-2));
-            a.style.left = (l-20) + "px";
+            a.style.left = (l-30) + "px";
             if (l > 0) {
               requestAnimationFrame(f);
             }
