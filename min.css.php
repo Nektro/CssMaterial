5cba57245450db65a0f1ac79d8472603875d41bd
Nektro <nektromc@gmail.com>
2016-02-28 18:22:31 -0500

    switch from spaces to tabs

--- min.css.php
+++ min.css.php
@@ -1,14 +1,14 @@
 <?php
 function fileGetAll($fa) {
-  $result = "";
-  foreach ($fa as $file) {
-    $result .= file_get_contents($file);
-  }
-  return $result;
+    $result = "";
+    foreach ($fa as $file) {
+        $result .= file_get_contents($file);
+    }
+    return $result;
 }
 function minifiyCss($css) {
-  // credit: http://manas.tungare.name/software/css-compression-in-php/
-  return str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', str_replace(': ', ':', preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css)));
+    // credit: http://manas.tungare.name/software/css-compression-in-php/
+    return str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', str_replace(': ', ':', preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $css)));
 }
 
 header("Content-type: text/css");
@@ -16,15 +16,15 @@ echo "@import 'https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.
 echo "@import 'https://fonts.googleapis.com/css?family=Material+Icons|Roboto:300,400,500';\n";
 echo "@import 'https://rawgit.com/fians/Waves/master/dist/waves.css';\n";
 echo minifiyCss(fileGetAll(array(
-  'src/components/header.css',
-  'src/components/fab.css',
-  'src/components/button.css',
-  'src/components/datatable.css',
-  'src/components/card.css',
-  'src/components/textfield.css',
-  'src/extras/sidenav.css',
-  'src/extras/footer.css',
-  'src/color.css',
-  'src/grid.css',
-  'src/general.css'
+    'src/components/header.css',
+    'src/components/fab.css',
+    'src/components/button.css',
+    'src/components/datatable.css',
+    'src/components/card.css',
+    'src/components/textfield.css',
+    'src/extras/sidenav.css',
+    'src/extras/footer.css',
+    'src/color.css',
+    'src/grid.css',
+    'src/general.css'
 )));
--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 (function() {
-  // add overlay div
-  document.body.innerHTML += "<div id='overlay'></div>";
-  // slide out navbar
-  var a = document.getElementById("side-nav") || document.createElement('div');
-  if (a.className === "pull-out") {
-    document.getElementsByClassName("sidenavcontrol")[0].addEventListener('click', function(e) {
-      var f = function() {
-        var b = window.getComputedStyle(a, null).getPropertyValue("left");
-        var l = parseInt(b.substring(0,b.length-2));
-        a.style.left = (l+30) + "px";
-        if (l < 240) {
-          requestAnimationFrame(f);
-        }
-        else {
-          a.style.left = "240px";
-        }
-      };
-      document.getElementById('overlay').style.display = "block";
-      f();
-    });
-    document.getElementsByTagName("body")[0].addEventListener('click', function(e) {
-      if (window.getComputedStyle(a, null).getPropertyValue("left") === "240px") {
-        if (e.pageX > 240) {
-          var f = function() {
-            var b = window.getComputedStyle(a, null).getPropertyValue("left");
-            var l = parseInt(b.substring(0,b.length-2));
-            a.style.left = (l-30) + "px";
-            if (l > 0) {
-              requestAnimationFrame(f);
+    // add overlay div
+    document.body.innerHTML += "<div id='overlay'></div>";
+    // slide out navbar
+    var a = document.getElementById("side-nav") || document.createElement('div');
+    if (a.className === "pull-out") {
+        document.getElementsByClassName("sidenavcontrol")[0].addEventListener('click', function(e) {
+            var f = function() {
+                var b = window.getComputedStyle(a, null).getPropertyValue("left");
+                var l = parseInt(b.substring(0,b.length-2));
+                a.style.left = (l+30) + "px";
+                if (l < 240) {
+                    requestAnimationFrame(f);
+                }
+                else {
+                    a.style.left = "240px";
+                }
+            };
+            document.getElementById('overlay').style.display = "block";
+            f();
+        });
+        document.getElementsByTagName("body")[0].addEventListener('click', function(e) {
+            if (window.getComputedStyle(a, null).getPropertyValue("left") === "240px") {
+                if (e.pageX > 240) {
+                    var f = function() {
+                        var b = window.getComputedStyle(a, null).getPropertyValue("left");
+                        var l = parseInt(b.substring(0,b.length-2));
+                        a.style.left = (l-30) + "px";
+                        if (l > 0) {
+                            requestAnimationFrame(f);
+                        }
+                        else {
+                            a.style.left = "0px";
+                        }
+                    };
+                    document.getElementById('overlay').style.display = "none";
+                    f();
+                }
             }
-            else {
-              a.style.left = "0px";
-            }
-          };
-          document.getElementById('overlay').style.display = "none";
-          f();
-        }
-      }
-    });
-  }
-  // button waves effect
-  Waves.attach('.btn');
-  Waves.init();
+        });
+    }
+    // button waves effect
+    Waves.attach('.btn');
+    Waves.init();
 })();
