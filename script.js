e4efc3e100313ef28ee5decc8bbd8a0442dfbd15
Nektro <nektromc@gmail.com>
2016-05-24 16:19:50 -0400

    use CSS transformations for side nav instead of JS

--- script.js
+++ script.js
@@ -23,36 +23,14 @@
     var a = document.getElementById("side-nav") || document.createElement('div');
     if (a.className === "pull-out") {
         document.getElementsByClassName("sidenavcontrol")[0].addEventListener('click', function(e) {
-            var f = function() {
-                var b = window.getComputedStyle(a, null).getPropertyValue("left");
-                var l = parseInt(b.substring(0,b.length-2));
-                a.style.left = (l+30) + "px";
-                if (l < 240) {
-                    requestAnimationFrame(f);
-                }
-                else {
-                    a.style.left = "240px";
-                }
-            };
-            document.getElementById('overlay').style.display = "block";
-            f();
+            a.setAttribute('aria-expanded', 'true');
         });
         document.getElementsByTagName("body")[0].addEventListener('click', function(e) {
-            if (window.getComputedStyle(a, null).getPropertyValue("left") === "240px") {
-                if (e.pageX > 240) {
-                    var f = function() {
-                        var b = window.getComputedStyle(a, null).getPropertyValue("left");
-                        var l = parseInt(b.substring(0,b.length-2));
-                        a.style.left = (l-30) + "px";
-                        if (l > 0) {
-                            requestAnimationFrame(f);
-                        }
-                        else {
-                            a.style.left = "0px";
-                        }
-                    };
-                    document.getElementById('overlay').style.display = "none";
-                    f();
+            if (e.path.indexOf(document.getElementsByClassName("sidenavcontrol")[0]) === -1) {
+                if (e.path.indexOf(a) === -1) {
+                    if (a.getAttribute('aria-expanded') === 'true') {
+                        a.setAttribute('aria-expanded', 'false');
+                    }
                 }
             }
         });
